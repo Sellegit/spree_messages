698f1fd74e95806ae08a1f8ab55bc516ea388c95
simple <simple.continue@gmail.com>
2017-05-18 20:47:35 +0800

    message_image

--- app/models/spree/message_image.rb
+++ app/models/spree/message_image.rb
@@ -1,8 +1,41 @@
-class Spree::MessageImage < Spree::Image
-  attachment_definitions[:attachment][:styles] = {
-    universal: '800x800>'
-  }
-  attachment_definitions[:attachment][:default_style] = :universal
-  attachment_definitions[:attachment].delete(:url)
-  attachment_definitions[:attachment].delete(:path)
+module Spree
+  class MessageImage < Asset
+    validate :no_attachment_errors
+
+    has_attached_file :attachment,
+                      styles: {universal: '800x800>'},
+                      default_style: :product,
+                      convert_options: { all: '-strip -auto-orient -interlace Plane -quality 80' } #  -colorspace sRGB' }
+    validates_attachment :attachment,
+      :presence => true,
+      :content_type => { :content_type => %w(image/jpeg image/jpg image/png image/gif) }
+
+    # save the w,h of the original image (from which others can be calculated)
+    # we need to look at the write-queue for images which have not been saved yet
+    after_post_process :find_dimensions
+
+    #used by admin products autocomplete
+    def mini_url
+      attachment.url(:small, false)
+    end
+
+    def find_dimensions
+      temporary = attachment.queued_for_write[:original]
+      filename = temporary.path unless temporary.nil?
+      filename = attachment.path if filename.blank?
+      geometry = Paperclip::Geometry.from_file(filename)
+      self.attachment_width  = geometry.width
+      self.attachment_height = geometry.height
+    end
+
+    # if there are errors from the plugin, then add a more meaningful message
+    def no_attachment_errors
+      unless attachment.errors.empty?
+        # uncomment this to get rid of the less-than-useful interim messages
+        # errors.clear
+        errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
+        false
+      end
+    end
+  end
 end
